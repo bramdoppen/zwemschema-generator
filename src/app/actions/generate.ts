417e695dc0af997cfe43144bbebdcf3e1de65846
Bram Doppen <bramdoppen@gmail.com>
2024-09-29 19:18:25 +0200

    Werk project bij

--- src/app/actions/generate.ts
+++ src/app/actions/generate.ts
@@ -30,7 +30,9 @@ const workoutSchema = z.object({
 
 export async function generate(prompt: string) {
   if (await hasReachedLimit()) {
-    throw new Error("U heeft de limiet van 20 gegenereerde schema's bereikt.");
+    throw new Error(
+      "U heeft de limiet van 30 gegenereerde schema's per dag bereikt."
+    );
   }
 
   await incrementGenerations();
@@ -39,9 +41,9 @@ export async function generate(prompt: string) {
 
   (async () => {
     const { partialObjectStream } = await streamObject({
-      model: openai("gpt-4o"),
+      model: openai("gpt-4o-mini"),
       system:
-        "Je genereert een gestructureerd zwemschema op basis van de input van de gebruiker. Gebruik alleen afstanden in meters en geef rusttijden aan tussen de oefeningen.",
+        "Je bent een ervaren zwemcoach die gedetailleerde en gepersonaliseerde zwemschema's genereert. Maak een gestructureerd trainingsplan op basis van de input van de gebruiker. Houd rekening met het opgegeven vaardigheidsniveau, focus en gewenste afstand. Gebruik alleen afstanden in meters en geef specifieke rusttijden aan in seconden tussen de oefeningen. Zorg ervoor dat elke sectie van het schema (inzwemmen, kernsets, uitzwemmen) duidelijk is gedefinieerd met passende oefeningen en intensiteit. Pas de moeilijkheidsgraad en variatie aan op basis van het niveau van de zwemmer.",
       prompt: prompt,
       schema: workoutSchema,
     });
--- src/lib/userLimits.ts
+++ src/lib/userLimits.ts
@@ -8,11 +8,7 @@ const COOKIE_NAME = "schema_generations";
 export async function incrementGenerations(): Promise<number> {
   const cookieStore = cookies();
   const current = parseInt(cookieStore.get(COOKIE_NAME)?.value || "0", 10);
-  console.log(`Current cookie value: ${cookieStore.get(COOKIE_NAME)?.value}`);
   const newCount = current + 1;
-
-  console.log(`Huidig generatie aantal: ${current}`);
-  console.log(`Nieuw generatie aantal: ${newCount}`);
 
   cookies().set({
     name: COOKIE_NAME,
@@ -24,10 +20,6 @@ export async function incrementGenerations(): Promise<number> {
     maxAge: 24 * 60 * 60, // 24 uur in seconden
   });
 
-  // Note: De volgende lijn zal niet de bijgewerkte cookie weergeven
-  const updatedCookie = cookieStore.get(COOKIE_NAME);
-  console.log(`Updated cookie: ${updatedCookie?.value}`);
-
   return newCount;
 }
 
@@ -37,6 +29,5 @@ export async function getGenerationsCount(): Promise<number> {
 }
 
 export async function hasReachedLimit(): Promise<boolean> {
-  console.log("hasReachedLimit", await getGenerationsCount());
   return (await getGenerationsCount()) >= MAX_GENERATIONS;
 }
